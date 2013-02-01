a8949dcfc723e9069a6dc3349582b020554645cc
Brennan Vincent <brennan.vincent@gmail.com>
2013-01-31 20:56:19 -0700

    fake getPossibleSponsorsByCommittee

--- resolution.js
+++ resolution.js
@@ -130,6 +130,12 @@ function deleteOperative(index)
 	$(".operative").gt(index - 1).each(bindOperative);
 }
 
+function getPossibleSponsorsByCommittee(committeeId)
+{
+	//FIXME: fake.
+	return ["Bahrain", "USA", "UK", "Russian Federation", "France", "India", "Québec câliss", "The Federal Republic of Cascadia and Alaska"];
+}
+
 function populateResolution(resolution)
 {
 	$("#preambulars").empty();
