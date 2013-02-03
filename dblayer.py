f81b33363535ecc6a4b67b020b209592244c565e
Brennan Vincent <brennan.vincent@gmail.com>
2013-02-03 01:13:05 -0700

    dblayer.save

--- dblayer.py
+++ dblayer.py
@@ -1,5 +1,6 @@
 from google.appengine.ext import db
 import User
+import json
 
 class NoSuchUserError(Exception):
     pass
@@ -9,13 +10,14 @@ class NoSuchResolutionError(Exception):
 
 def _ResolutionModel(db.model):
     ownerId = db.IntegerProperty()
-    serializedResolutionObject = db.TextObject()
+    serializedResolutionObject = db.TextProperty()
     committeeId = db.IntegerProperty()
     status = db.IntegerProperty()
     index = db.IntegerProperty()
     topicIndex = db.IntegerProperty()
     assigneeId = db.IntegerProperty()
     originalAssigneeId = db.IntegerProperty()
+    comments = db.TextProperty()
 
 def _UserModel(db.Model):
     email = db.EmailProperty()
@@ -43,3 +45,28 @@ def getWebbotUserByEmail(email):
 def delete(resolutionId):
     k = db.Key.from_path('_ResolutionModel', resolutionId)
     db.delete(k)
+
+def save(ri):
+    if (ri.resolutionId != None):
+        m = _ResolutionModel(key = db.Key.from_path('_ResolutionModel', ri.resolutionId),
+                ownerId = ri.ownerId,
+                serializedResolutionObject = json.dumps(ri.resolution),
+                committeeId = ri.committeeId,
+                status = ri.status,
+                index = ri.index,
+                topicIndex = ri.topic
+                assigneeId = ri.assigneeId
+                originalAssigneeId = ri.originalAssigneeId
+                comments = ri.comments)
+    else:
+        m = _ResolutionModel(ownerId = ri.ownerId,
+                serializedResolutionObject = json.dumps(ri.resolution),
+                committeeId = ri.committeeId,
+                status = ri.status,
+                index = ri.index,
+                topicIndex = ri.topic
+                assigneeId = ri.assigneeId
+                originalAssigneeId = ri.originalAssigneeId
+                comments = ri.comments)
+    m.put()
+
