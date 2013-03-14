080c670c7d3a0ec9d53ccac41e710b17c0bba876
Brennan Vincent <brennan.vincent@gmail.com>
2013-03-14 04:14:37 -0700

    Send bilingual resolutions to Spanish RPC

--- dblayer.py
+++ dblayer.py
@@ -57,6 +57,8 @@ def _getFilterString(tup):
         return "`%s` IN (" % field + string.join(["%s"] * len(val), ", ") +")"
 
 def getRPC_ID(lang):
+    if lang == BILINGUAL:
+        lang = SPANISH
     cursor = _getCursor()
     cursor.execute("SELECT id FROM Users WHERE role=%s AND language=%s", (User.RPC_ROLE, lang))
     row = cursor.fetchone()
